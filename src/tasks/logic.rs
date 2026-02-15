00d114eab9545a43523da435eaafb2b32de75769
phantekzy <mainilotfi@gmail.com>
2026-02-16 00:52:53 +0100

    created the create_task function

--- src/tasks/logic.rs
+++ src/tasks/logic.rs
@@ -1,3 +1,14 @@
 use crate::tasks::models::{Priority, Task};
 
-pub fn create_task(id: u32, name: &str, p: Priority, desc: Option<&str>) -> Result<task, String> {}
+pub fn create_task(id: u32, name: &str, p: Priority, desc: Option<&str>) -> Result<task, String> {
+    if (name.is_empty()) {
+        return Err(String::from("Name cannot be Empty"));
+    }
+    Ok(Task {
+        id,
+        name: name.to_string(),
+        priority: p,
+        description: desc.map(|s| s.to_string()),
+        is_done: false,
+    })
+}
