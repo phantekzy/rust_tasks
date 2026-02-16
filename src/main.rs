32e1c9840e6d63ccb11e4d4e2fa93fa9dd6554aa
phantekzy <mainilotfi@gmail.com>
2026-02-16 01:05:58 +0100

    added a mid prio task

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,7 @@ fn main() {
     if let Ok(t) = create_task(1, "Install Rust", Priority::Hight, Some("Use rustup")) {
         todo_list.push(t);
     }
+    if let Ok(t) = create_task(2, "Learn Modules", Priority::Medium, None) {
+        todo_list.push(t);
+    }
 }
